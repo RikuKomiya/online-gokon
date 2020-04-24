dbaf0f737b8443fb37bc5aa2b8663de5121f79a6
Riku Komiya <824rikukomiya@gmail.com>
2020-04-24 17:23:40 +0900

    d

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,20 +2,12 @@ import React from 'react'
 import Container from '@material-ui/core/Container'
 import Typography from '@material-ui/core/Typography'
 import Box from '@material-ui/core/Box'
-import ProTip from '../layout/ProTip'
-import Link from '~/layout/Link'
-import Copyright from '~/layout/Copyright'
 
 export default function About() {
   return (
     <Container maxWidth="sm">
       <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          this is about page
-        </Typography>
-        <Link href="/">Go to the main page</Link>
-        <ProTip />
-        <Copyright />
+        <h1>about</h1>
       </Box>
     </Container>
   )
